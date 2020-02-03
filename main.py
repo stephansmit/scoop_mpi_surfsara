dd65f933058ca70deebfc0dfd9ce9ff38f8b8950
Stephan Smit <stephans@int2.bullx>
2020-02-03 13:11:03 +0100

    updated the cores

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def mpi_exec_function(casename, input_file, cores, exec_dir):
     flog = open(str(casename)+'.log', 'w')
     fout = open(str(casename)+'.out', 'w')
     
-    cmd = ["mpirun", "-np", str(cores),os.path.join(exec_dir,'run'), 'mpi_input.cfg']
+    cmd = ["mpirun", "-np", str(cores),os.path.join(exec_dir,'run'), 'mpi_input.cfg'] #run is the name of the executable
     p = subprocess.Popen(
             cmd, stdout=fout, stderr=ferr, cwd=os.getcwd())
     p.communicate()
@@ -30,5 +30,5 @@ if __name__ == "__main__":
     ncores_mpi= 4 #this is the number of cores for running the MPI-CODE
     ncases = 48   #Number of times the MPI-Code is called in total (nthreads in the batchjob will determine how many in parallel)
     cases = np.linspace(1,ncases,ncases-1, dtype=int)
-    returnValues = list(futures.map(partial(mpi_exec_function,input_file=mpi_input_file, cores=4, exec_dir=mpi_exec_dir),cases))
+    returnValues = list(futures.map(partial(mpi_exec_function,input_file=mpi_input_file, cores=ncores_mpi, exec_dir=mpi_exec_dir),cases))
 
